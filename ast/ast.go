d81ada6cfba15c772e3787d5123c4f4979c6fa85
Kamil2501 <kamilbibwiz@gmail.com>
2024-08-01 20:07:15 +0200

    implemented expressionNode() and TokenLiteral() methods for Identifier struct in ast.go

--- ast/ast.go
+++ ast/ast.go
@@ -44,3 +44,8 @@ type Identifier struct {
 	Token token.Token //token.IDENT
 	Value string
 }
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string { 
+	return i.Token.Literal 
+}
