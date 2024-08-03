88156dc5914acb9613f819c43a9828758328b10e
Kamil2501 <kamilbibwiz@gmail.com>
2024-08-03 17:06:50 +0200

    changed tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -6,67 +6,33 @@ import (
 	"testing"
 )
 
-func TestLetStatements(t *testing.T) {
+func TestReturnStatements(t *testing.T) {
 	input := `
-let x 5;
-let = 10;
-let 838383;
+return 5;
+return 10;
+return 993322;
 `
-
 	l := lexer.New(input)
 	p := New(l)
 
 	program := p.ParseProgram()
 	checkParseError(t, p)
 
-	if program == nil {
-		t.Fatalf("ParseProgram() returned nil")
-	}
-
 	if len(program.Statements) != 3 {
-		t.Fatalf("program.Statements does not contain 3 statements got=%d", len(program.Statements))
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))	
 	}
 
-	tests := []struct{
-		expectedIdentifier string
-	}{
-		{"x"},
-		{"y"},
-		{"foobar"},
-	}
-
-	for i, tt := range tests {
-		stmt := program.Statements[i]
-		if !testLetStatements(t, stmt, tt.expectedIdentifier) {
-			return
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got %q",
+			returnStmt.TokenLiteral())
 		}
 	}
-}
-
-func testLetStatements(t *testing.T, s ast.Statement, name string) bool {
-	if s.TokenLiteral() != "let" {
-			t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
-			return false
-	}
-
-	letStmt, ok := s.(*ast.LetStatement)
-
-	if !ok {
-		t.Errorf("s not *ast.LetStatement. got=%T", s)
-		return false
-	}
-
-	if letStmt.Name.Value != name {
-		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
-		return false
-	}
-
-	if letStmt.Name.TokenLiteral() != name {
-		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
-		return false
-	}
-	
-	return true
 }
 
 func checkParseError(t *testing.T, p *Parser) {
