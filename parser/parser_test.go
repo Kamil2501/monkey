04ef9b99fa333491f76407d0c1215bcd02803148
Kamil2501 <kamilbibwiz@gmail.com>
2024-08-03 16:51:01 +0200

    added checkParseError() function and using it in TestLetStatements() test function

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -17,6 +17,7 @@ let foobar = 838383;
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParseError(t, p)
 
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
@@ -66,4 +67,19 @@ func testLetStatements(t *testing.T, s ast.Statement, name string) bool {
 	}
 	
 	return true
+}
+
+func checkParseError(t *testing.T, p *Parser) {
+	errors := p.Errors()
+
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+
+	t.FailNow()
 }
