0be694499b05f48c3e944f0be7b5aef17592ff81
Kamil2501 <kamilbibwiz@gmail.com>
2024-08-10 22:58:06 +0200

    added bang operator support

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -21,6 +21,9 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBoolean(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -41,4 +44,26 @@ func nativeBoolToBoolean(input bool) *object.Boolean {
 	}
 
 	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperator(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperator(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return TRUE
+	}
 }
