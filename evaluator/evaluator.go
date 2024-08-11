211e128b014fa25dbbac72c6c56d9740644298af
Kamil2501 <kamilbibwiz@gmail.com>
2024-08-11 22:00:54 +0200

    added `isError` function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -184,3 +184,11 @@ func isTruthy(obj object.Object) bool {
 func newError(format string, a ...interface{}) *object.Error {
 	return &object.Error{Message: fmt.Sprintf(format, a...)}
 }
+
+func isError(obj object.Object) bool {
+	if obj != nil {
+		return obj.Type() == object.ERROR_OBJ
+	}
+
+	return false
+}
