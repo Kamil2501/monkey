a781fc033990afd3850027dccea0c1092bd56fe1
Kamil2501 <kamilbibwiz@gmail.com>
2024-08-15 21:30:57 +0200

    parser now can parse strings

--- parser/parser.go
+++ parser/parser.go
@@ -404,6 +404,10 @@ func (p *Parser) parseCallArguments() []ast.Expression {
 	return args
 }
 
+func (p *Parser) parseStringLiteral() ast.Expression {
+	return &ast.StringLiteral{Token: p.curToken, Value: p.curToken.Literal}
+}
+
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l, errors: []string{}}
 	//set curToken and peekToken
@@ -436,6 +440,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 
 	p.registerInfix(token.LPAREN, p.parseCallExpression)
+
+	p.registerPrefix(token.STRING, p.parseStringLiteral)
 
 	return p
 }
