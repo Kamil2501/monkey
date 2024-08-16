537564e8badbdcbed3bb2072326db47bb748232b
Kamil2501 <kamilbibwiz@gmail.com>
2024-08-16 19:55:29 +0200

    added support for built in functions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -240,6 +240,10 @@ func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object
 		return newError("identifier not found: " + node.Value)
 	}
 
+	if builtin, ok := builtins[node.Value]; ok {
+		return builtin
+	}
+
 	return val
 }
 
@@ -270,14 +274,16 @@ func isError(obj object.Object) bool {
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok {
+	switch fn := fn.(type) {
+	case *object.Function:
+		extendedEnv := extendFunctionEnv(fn, args)
+		evaluated := Eval(fn.Body, extendedEnv)
+		return unwrapReturnValue(evaluated)
+	case *object.Builtin:
+		return fn.Fn(args...)
+	default:
 		return newError("not a function: %s", fn.Type())
 	}
-
-	extendedEnv := extendFunctionEnv(function, args)
-	evaluated := Eval(function.Body, extendedEnv)
-	return unwrapReturnValue(evaluated)
 }
 
 func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
