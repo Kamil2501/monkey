946aeb3143cc261b9e87e36b7c8d18c60f453e24
Kamil2501 <kamilbibwiz@gmail.com>
2024-08-16 20:06:55 +0200

    extended testcase

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -28,6 +28,7 @@ if (5 < 10) {
 10 != 9;
 "foobar"
 "foo bar"
+[1, 2];
 `
 
 	tests := []struct {
@@ -108,7 +109,13 @@ if (5 < 10) {
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
 		{token.STRING, "foobar"},
-		{token.STRING, "foo bar"},
+		{token.STRING, "foo bar"},	
+		{token.LBRACKET, "["},
+		{token.INT, "1"},
+		{token.COMMA, ","},
+		{token.INT, "2"},
+		{token.RBRACKET, "]"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
