1c056570d279fa7c8abf155da3cce0a196855bbc
Kamil2501 <kamilbibwiz@gmail.com>
2024-08-16 20:02:03 +0200

    fixed `evalIdentifier` function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -235,16 +235,15 @@ func isTruthy(obj object.Object) bool {
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
-	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("identifier not found: " + node.Value)
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
 
 	if builtin, ok := builtins[node.Value]; ok {
 		return builtin
 	}
 
-	return val
+	return newError("identifier not found: " + node.Value)
 }
 
 func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
